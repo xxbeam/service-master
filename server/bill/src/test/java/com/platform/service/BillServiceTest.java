03ef9669ab76fbe56813c0ff73a3f682518f8da4
xiongxin <xiongxin@songxiaocai.com>
2019-11-12 19:27:19 +0800

    单测

--- server/bill/src/test/java/com/platform/service/BillServiceTest.java
+++ server/bill/src/test/java/com/platform/service/BillServiceTest.java
@@ -1,12 +1,7 @@
-package com.platform.controller;
+package com.platform.service;
 
 import com.alibaba.fastjson.JSON;
-import com.platform.base.RetCode;
 import com.platform.bill.service.BillBookService;
-import com.platform.dto.bill.BillBookDTO;
-import com.platform.util.ResultUtil;
-import com.platform.util.jwt.JwtUserConst;
-import com.platform.vo.JsonResult;
 import com.platform.vo.bill.BillBookTypeVo;
 import org.junit.Assert;
 import org.junit.Test;
@@ -14,11 +9,8 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.web.bind.annotation.RequestBody;
 
-import javax.validation.Valid;
 import java.util.List;
 
 /**
---
+++ server/user/src/test/java/com/platform/service/UserServiceTests.java
@@ -0,0 +1,47 @@
+package com.platform.service;
+
+import com.alibaba.fastjson.JSON;
+import com.platform.dto.user.UserDTO;
+import com.platform.user.service.UserService;
+import com.platform.vo.user.UserVo;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author xiongxin
+ * @date 2019/11/12
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Rollback(false)
+public class UserServiceTests {
+
+    @Autowired
+    UserService userService;
+
+    @Test
+    public void getUserById(){
+        try {
+            UserVo vo = userService.getUserById(1);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void login(){
+        UserDTO userDTO = new UserDTO();
+        userDTO.setLoginName("xxBeam");
+        userDTO.setPassword("123456");
+        try {
+            userService.login(userDTO);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
